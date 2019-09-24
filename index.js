ba3c4caf73cfc06dc69077b039396853eb514596
bhavBains <deepbains26@gmail.com>
2019-09-23 17:17:23 -0700

    added script for heroku deployment

--- index.js
+++ index.js
@@ -19,5 +19,15 @@ app.use(bodyParser.json());
 //Routes
 require("./routes/api/employees")(app);
 
+//Heroku deployment env in Production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, console.log(`server is up and running on ${PORT}`));
